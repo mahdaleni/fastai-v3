5f564fd647dd28377ef118aaa91892b773b876b7
Arif <dewisandrax@gmail.com>
2020-02-21 22:11:33 +0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/file/d/1cXsvnIDuoHvza9vFgjxA18LGw7zujwtt/view?usp=sharing'
+export_file_name = 'best_model.h5'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['BrownSpot' 'Healthy' 'Hispa' 'LeafBlast']
 path = Path(__file__).parent
 
 app = Starlette()
